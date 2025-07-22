c0f2ad3ddd849815d03b9ed731c2b466e75d4c37
UniqueIam <abhimanyukumar83097@gmail.com>
2025-07-22 19:05:47 +0530

    ypdated cors

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ dotenv.config({});
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://invo-sync-frontend-giet.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials:true
 }))
 
 app.use(express.json());
