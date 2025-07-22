18bff01e334acf20cf1e6bad9debc65d9348ede6
UniqueIam <abhimanyukumar83097@gmail.com>
2025-07-22 18:53:49 +0530

    updated cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config({});
 const app = express();
 
 app.use(cors({
-    origin: 'invosync-frontend.vercel.ap',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
